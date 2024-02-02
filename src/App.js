4cc4cb684d6f38408a1ef25acd6f3682351ac0b6
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-02 18:17:27 +0200

    added app.css

--- src/App.js
+++ src/App.js
@@ -1,30 +1,35 @@
-import React from "react";
+import React from "react;";
 import "./App.css";
+import SearchCityForm from "./SearchCityForm";
 
-export default function App() {
+function App() {
   return (
-    <div className="App">
-      <div className="container">
-        Weather App
-        <footer>
-          This project is coded by{" "}
-          <a
-            href="https://github.com/1543-STACK/climate-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            Anu Govender
-          </a>{" "}
-          and is{" "}
-          <a
-            href="https://github.com/1543-STACK/climate-react"
-            target="_blank"
-            rel="noreferrer"
-          >
-            open-sourced on Github
-          </a>
-        </footer>
+    <div>
+      <div className="App mb-0">
+        <div className="container">
+          <SearchCityForm defaultCity="Somerset West" />
+        </div>
       </div>
+      <footer className="fw-bold text-center mt-0">
+        This project is coded by{" "}
+        <a
+          href="https://github.com/1543-STACK/climate-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Anu Govender
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/1543-STACK/climate-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
     </div>
   );
 }
+
+export default App;
