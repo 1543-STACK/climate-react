cec04dcb7f28eba9fa0da93ad6bf4a2ebd73aae3
1543-STACK <141954178+1543-STACK@users.noreply.github.com>
2024-02-02 14:56:09 +0200

    Added Github repo url

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      Weather App
+      <footer>
+        This project is coded by{" "}
+        <a
+          href="https://github.com/1543-STACK/climate-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Anu Govender
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/1543-STACK/climate-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on Github
+        </a>
+      </footer>
+    </div>
+  );
 }
